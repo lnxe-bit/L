65045f749cc722c4270980e4a9872b290cca14f3
lnxe-bit <lance.yzn@gmail.com>
2023-08-15 01:17:59 +0800

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+
+
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold font-signature">lnxe</h1>
+      <NavBar /> 
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeroImage from "../assets/heroImage.png";
+import {MdOutlineKeyboardArrowRight} from "react-icons/md";
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
