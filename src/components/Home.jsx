72a884e961f0a426e5782971e7fef379d2972405
lnxe-bit <lance.yzn@gmail.com>
2023-08-15 02:13:02 +0800

    add content & button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,34 @@ import HeroImage from "../assets/heroImage.png";
 import {MdOutlineKeyboardArrowRight} from "react-icons/md";
 const Home = () => {
   return (
-    <div>Home</div>
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+            <div className="flex flex-col justify-center h-full">
+                <h2 className="text-4xl sm:text-7xl font-bold text-white">
+                    Hello, I'am Front-end Developer.
+                </h2>
+                <p className="text-gray-500 py-4 max-w-md">
+                    I maybe slow to respond but I love to work on web application using my techstack.
+                </p>
+                <div>
+                    <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+                        Projects 
+                        <span className="group-hover:rotate-90 duration-300">
+                            <MdOutlineKeyboardArrowRight className="ml-1" size={20} /> 
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div>
+                <img 
+                src={HeroImage} 
+                alt="my profile"
+                className="rounded-2xl mx-auto w-2/3 md:w-full"
+                />
+            </div>
+
+        </div>
+    </div>
   )
 }
 
