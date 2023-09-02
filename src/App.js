b73895aa7a1eaee3e4d1eb22f89d2d8972d9576d
lnxe-bit <lance.yzn@gmail.com>
2023-09-02 23:33:24 +0800

    Add about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 
 function App() {
@@ -8,7 +10,8 @@ function App() {
     <div>
       <NavBar /> 
       <Home />
-      
+      <About />
+      <Portfolio />
       <SocialLinks />
     </div>
   );
