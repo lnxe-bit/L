096b1cbebe129fcdce6eb00dbc9f7581d3090690
lnxe-bit <lance.yzn@gmail.com>
2023-09-08 17:03:29 +0800

    Add smooth scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroImage from "../assets/heroImage.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 const Home = () => {
   return (
     <div
@@ -17,12 +18,12 @@ const Home = () => {
             my techstack.
           </p>
           <div>
-            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
               Projects
               <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight className="ml-1" size={20} />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -20,12 +20,10 @@ const Portfolio = () => {
       id: 3,
       src: navbar,
     },
-
     {
       id: 4,
       src: reactSmooth,
     },
-
     {
       id: 5,
       src: installNode,
@@ -38,7 +36,7 @@ const Portfolio = () => {
 
   return (
     <div
-      name="Portfolio"
+      name="portfolio"
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
@@ -46,8 +44,9 @@ const Portfolio = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
           </p>
-          <p className="py-6">Checkout some of my work here!</p>
+          <p className="py-6">Check out some of my work right here</p>
         </div>
+
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
           {portfolios.map(({ id, src }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
@@ -56,7 +55,7 @@ const Portfolio = () => {
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
-              <div className="flex justify-center items-center">
+              <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Demo
                 </button>
